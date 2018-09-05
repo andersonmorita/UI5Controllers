c0275085e16a4aa33888879c9dd8bacf6a4d78de
P225542 <anderson.morita@gmail.com>
2018-09-05 10:48:56 +0000

    UI5 Controllers first commit

---
+++ webapp/controller/View1Controllers.controller.js
@@ -0,0 +1,29 @@
+/*sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("NamespaceControllers.PNameControllers.controller.View1Controllers", {
+
+	});
+});*/
+/*meuss codigo*/
+/*sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
+   return Controller.extend("", {
+   });
+});*/
+/*meuss codigo*/
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onShowHello : function () {
+         // show a native JavaScript alert
+         alert("Hello World");
+      }
+   });
+});
